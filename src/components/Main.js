bd9bdcab22c8ae7856c0b4ed00dbc94c8d99e445
Wayhito <moskariko@mail.ru>
2023-04-30 03:40:00 +0200

    Fixes: внешний вид кода + ImagePopup

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,67 +3,59 @@ import { api } from "../utils/Api.js";
 import Card from "./Card.js";
 
 function Main(props) {
-    const [userName, setUserName] = React.useState('');
-    const [userDescription, setUserDescription] = React.useState('');
-    const [userAvatar, setUserAvatar] = React.useState('');
-    const [cards, setInitialCards] = React.useState([]);
+  const [userName, setUserName] = React.useState("");
+  const [userDescription, setUserDescription] = React.useState("");
+  const [userAvatar, setUserAvatar] = React.useState("");
+  const [cards, setInitialCards] = React.useState([]);
 
-    React.useEffect(() => {
-        Promise.all([
-          api.getUserInfo(),
-          api.getInitialCards()
-        ])
+  React.useEffect(() => {
+    Promise.all([api.getUserInfo(), api.getInitialCards()])
 
-          .then((values) => {
-            setUserName(values[0].name);
-            setUserDescription(values[0].about);
-            setUserAvatar(values[0].avatar);
-            setInitialCards([...values[1]]);
-          })
+      .then((values) => {
+        setUserName(values[0].name);
+        setUserDescription(values[0].about);
+        setUserAvatar(values[0].avatar);
+        setInitialCards([...values[1]]);
+      })
 
-          .catch((err) => {console.log(err)});
-    }, []);
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-    return (
-        <>
-            <main>
-                <section className="profile">
+  return (
+    <main>
+      <section className="profile">
+        <div className="profile__image-container">
+          <img className="profile__image" src={userAvatar} alt="Картинка профиля"/>
+          <button className="profile__image-button" onClick={props.onEditAvatar}
+          ></button>
+        </div>
 
-                    <div className="profile__image-container">
-                        <img className="profile__image" src={userAvatar} alt="Картинка профиля"/>
-                        <button className="profile__image-button" onClick={props.onEditAvatar}></button>
-                    </div>
+        <div className="profile__info">
+          <h1 className="profile__name">{userName}</h1>
+          <button className="profile__edit-button" onClick={props.onEditProfile}>
+            <img className="profile__pen" src={require("../images/editbuttonpen.svg").default} alt="редактировать"/>
+          </button>
+          <p className="profile__job">{userDescription}</p>
+        </div>
 
-                    <div className="profile__info">
-                        <h1 className="profile__name">{userName}</h1>
-                        <button className="profile__edit-button" onClick={props.onEditProfile}>
-                            <img className="profile__pen" src={require('../images/editbuttonpen.svg').default} alt="редактировать"/>
-                        </button>
-                        <p className="profile__job">{userDescription}</p>
-                    </div>
+        <button className="profile__add-button" onClick={props.onNewLocation}>
+          <img className="profile__plus" src={require("../images/addbutton.svg").default} alt="добавить"/>
+        </button>
+      </section>
 
-                    <button className="profile__add-button" onClick={props.onNewLocation}>
-                        <img className="profile__plus" src={require('../images/addbutton.svg').default} alt="добавить"/>
-                    </button>
+      <section className="elements">
+        {cards.map((card) => (
+          <li key={card._id} className="element">
+            <Card card={card} onCardClick={props.onCardClick}/>
+          </li>
+        ))}
+      </section>
+    </main>
+  );
+}
 
-                </section>
-
-                <section className="elements">
-
-                {
-                cards.map((card) =>
-                <li key={card._id} className="element">
-                  <Card card={card} onCardClick={props.onCardClick} />
-                </li>
-                )
-                }
-
-                </section>
-            </main>
-        </>
-    );
-  }
-  
 export default Main;
 
 // {/* <li className="location">
